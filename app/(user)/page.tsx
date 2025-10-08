d0ed83ce321ee6c5567baf071bbb045353e14b6b
Patrick Murani <patrickmurani@gmail.com>
2025-10-08 14:00:04 +0300

    File update to patrickmurani

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
                 className="mt-4"
               >
                 <Link 
-                  href={"Murani-Ndwiga.pdf"} 
+                  href={"patrickmurani.pdf"} 
                   target="_blank" 
                   rel="noopener noreferrer" 
                   className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
